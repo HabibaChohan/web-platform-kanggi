f16deaba9f99835cba90670ca8553a570095b72c
HabibaChohan <132688647+HabibaChohan@users.noreply.github.com>
2023-06-02 20:03:28 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-kanggi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-kanggi)
---
+++ script.js
@@ -0,0 +1,9 @@
+/** Implement a click function to which writes digits on calculator screen */
+
+/** Implement a function which clears digits from calculator screen upon clicking AC button */
+
+/** Implement a function which enters digit to screen upon pressing any number button and decimal point */
+
+/** Implement a function for adding/subtracting/multiplying/dividing first entered number */
+
+/** */
